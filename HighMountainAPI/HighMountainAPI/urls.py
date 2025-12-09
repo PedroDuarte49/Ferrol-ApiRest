40204962aa28efd87aa7ea9b412fafc2693bdaf2
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2025-12-09 12:26:00 +0100

    Creacion Endpoint Login

--- HighMountainAPI/HighMountainAPI/urls.py
+++ HighMountainAPI/HighMountainAPI/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from highmountainapp import endpoints
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/login/', endpoints.login_user),
 
 ]
---
+++ HighMountainAPI/highmountainapp/endpoints.py
@@ -0,0 +1,30 @@
+import secrets
+
+import bcrypt
+from django.http import JsonResponse
+import json
+
+from highmountainapp.models import CustomUser, UserSession
+
+
+def login_user(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'HTTP method unsupported'}, status=405)
+    body_json = json.loads(request.body)
+    json_username = body_json['user_username']
+    json_password = body_json['user_password']
+    try:
+        db_user = CustomUser.objects.get(email=json_username)
+    except CustomUser.DoesNotExist:
+        pass  # No existe el usuario. En la siguiente tarea lo gestionamos
+
+    if bcrypt.checkpw(json_password.encode('utf8'), db_user.encrypted_password.encode('utf8')):
+        random_token = secrets.token_hex(10)
+        session = UserSession(user=db_user, token=random_token)
+        session.save()
+        return JsonResponse({"token": random_token}, status=201)
+
+    else:
+        pass  # Contraseña incorrecta. En la siguiente tarea lo gestionamos
+
+    return JsonResponse({'message': 'User logged in successfully'})
