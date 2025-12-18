80897b4ba124db37d8f15df47835e77e319d9297
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2025-12-18 10:33:04 +0100

    fixed models, lets see if i f*%#ed up

--- HighMountainAPI/highmountainapp/models.py
+++ HighMountainAPI/highmountainapp/models.py
@@ -8,4 +8,19 @@ class UserSession(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     token = models.CharField(unique=True, max_length=30)
 
+class Score(models.Model):
+    id = models.AutoField(primary_key=True)
+    player = models.CharField(max_length=15)
+    points = models.IntegerField()
 
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    message = models.TextField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    foro = models.ForeignKey(Score, on_delete=models.CASCADE)
+
+class Foro(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=15)
+    comments = models.ManyToManyField(Comment)
