943a5a1fa2c2fd8f9dca4cd0f8011aeaa9ed1de5
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2026-01-08 09:12:18 +0100

    Creacion Endpoint Register

--- HighMountainAPI/highmountainapp/endpoints.py
+++ HighMountainAPI/highmountainapp/endpoints.py
@@ -29,3 +29,21 @@ def login_user(request):
 
     else:
         return JsonResponse({"error": "Invalid password"}, status=401)
+
+def register_user(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'HTTP method unsupported'}, status=405)
+    body_json = json.loads(request.body)
+    required_fields = ['user_email', 'user_password']
+    if not all(field in body_json for field in required_fields):
+        return JsonResponse({'error': 'Missing parameter'}, status=400)
+    json_username = body_json['user_username']
+    json_password = body_json['user_password']
+    try:
+        CustomUser.objects.get(email=json_username)
+        return JsonResponse({"error": "User already exists"}, status=409)
+    except CustomUser.DoesNotExist:
+        encrypted_password = bcrypt.hashpw(json_password.encode('utf8'), bcrypt.gensalt())
+        new_user = CustomUser(username=json_username, encrypted_password=encrypted_password.decode('utf8'))
+        new_user.save()
+        return JsonResponse({"message": "User created successfully"}, status=200)
